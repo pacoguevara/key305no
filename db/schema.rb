76ff1b23ae8962ee17112ed8de5068d6d440f5af
Paco Guevara <pacoguevaraa@gmail.com>
2016-10-21 01:10:41 -0500

    alter users table, create clinics table and add association

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160701070133) do
+ActiveRecord::Schema.define(version: 20161021060442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clinics", force: true do |t|
+    t.string "name"
+    t.string "address"
+    t.string "phone"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -29,6 +35,12 @@ ActiveRecord::Schema.define(version: 20160701070133) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "address"
+    t.string   "phone"
+    t.string   "role"
+    t.integer  "clinic_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
