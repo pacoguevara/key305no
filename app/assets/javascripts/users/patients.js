7e4f1dfa7f7a2496a6fc311aed74adb4c17f95fe
Paco Guevara <pacoguevaraa@gmail.com>
2016-10-22 01:24:43 -0500

    handle save button click

--- app/assets/javascripts/users/patients.js
+++ app/assets/javascripts/users/patients.js
@@ -7,6 +7,16 @@ Patient = (function(){
 
   var bind = function(){
     $('.modal-trigger').leanModal();
+    $('#save-new-patient').click(hdlSaveNewPatient);
+  }
+
+  var hdlSaveNewPatient = function(){
+    var firstName = $('#first_name').val();
+    var lastName = $('#last_name').val();
+    var email = $('#email').val();
+    var phone = $('#phone').val();
+    var address = $('#address').val();
+    console.log(firstName+lastName+email+phone+address);
   }
 
   return {
