5db2d38f5e1bb1d700e454515ca3024d4cfbe2b3
Paco Guevara <pacoguevaraa@gmail.com>
2016-10-25 21:48:51 -0500

    namespace and route to users resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
   devise_for :users
   root :to => 'pages#home'
@@ -5,4 +7,11 @@ Rails.application.routes.draw do
 
   # Users Controller
   get 'patients' => 'users#patients'
+
+  # API V1
+  namespace :api, defaults: {format: 'json'} do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      resources :users 
+    end
+  end
 end
