361b8471b5aa491ccb450ec170d6943c42ac191f
Paco Guevara <pacoguevaraa@gmail.com>
2016-10-25 21:45:32 -0500

    create function to add new patient

--- app/assets/javascripts/users/user.js
+++ app/assets/javascripts/users/user.js
@@ -3,7 +3,21 @@ var User;
 User = (function(){
   'use strict';
 
+  var url = '/api/users/';
+
+  var newPatient = function(params, scs, err){
+    params.role = 'patient'
+		$.ajax({
+      type : "POST",
+      url : url,
+      dataType : "json",
+			data: params,
+      success : scs,
+      error : err
+    });
+  }
+
   return {
-    
+    newPatient: newPatient
   }
 }());
