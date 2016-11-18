fb1dfc595a6c44f303bd047844d94053bf12c9f3
Paco Guevara <pacoguevaraa@gmail.com>
2016-11-18 00:47:26 -0600

    run migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161026033042) do
+ActiveRecord::Schema.define(version: 20161118062823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20161026033042) do
     t.string   "role"
     t.integer  "clinic_id"
     t.integer  "nutritionist_id"
+    t.boolean  "active"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
